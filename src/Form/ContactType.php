ec3d4cf68e0e1460be48c0e15561f017efa0ebd6
LDorine <dodosav.fr@gmail.com>
2019-07-19 11:03:22 +0200

    Adding contact

---
+++ src/Form/ContactType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('mail')
+            ->add('sujet', ChoiceType::class, [
+                'choice' => [
+                    'Informations' => 'informations',
+                    'Questions' => 'questions',
+                    'Réservations groupe' => 'groupe',
+                ],
+                'Label' => 'Choissiez un sujet'
+            ])
+            ->add('commentaire', TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
